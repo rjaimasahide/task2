aeaf3c6d325a943684b313bbf29e69bd9f36644f
EC2 Default User <ec2-user@ip-172-31-38-10.ap-northeast-1.compute.internal>
2022-12-19 08:48:19 +0000

    テスト　追加

--- test/controllers/homes_controller_test.rb
+++ test/controllers/homes_controller_test.rb
@@ -4,9 +4,10 @@ class HomesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
-  
+
   test "get homes" do
     get root_url
     assert_response :success
+    assert_select "h1", "ようこそBookersへ！"
   end
 end
